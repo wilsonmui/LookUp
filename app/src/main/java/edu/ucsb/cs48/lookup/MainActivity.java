86dcd991fc6a4507ee7b3e07550fd6f52ad36ea1
Erick Suarez <mr.ericksuarez@gmail.com>
2018-02-04 13:26:31 -0800

    Add Signed in check for onStart()

--- app/src/main/java/edu/ucsb/cs48/lookup/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/MainActivity.java
@@ -3,11 +3,30 @@ package edu.ucsb.cs48.lookup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        //TODO: Update UI accordingly
     }
 }
