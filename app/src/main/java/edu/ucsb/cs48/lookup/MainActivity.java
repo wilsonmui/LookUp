01c18c550470ad9e4a581019cbf9a5ba1872b3e1
Erick Suarez <mr.ericksuarez@gmail.com>
2018-02-04 20:56:58 -0800

    Add Classic Authentication, and Fix up xml files

--- app/src/main/java/edu/ucsb/cs48/lookup/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/MainActivity.java
@@ -1,32 +1,30 @@
 package edu.ucsb.cs48.lookup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    private FirebaseAuth mAuth;
-
+    //==============================================================================================
+    // On Create Setup
+    //==============================================================================================
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.get_started_button).setOnClickListener(this);
     }
-
+    //==============================================================================================
+    // Action Listeners
+    //==============================================================================================
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    private void updateUI(FirebaseUser currentUser) {
-        //TODO: Update UI accordingly
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.get_started_button:
+                startActivity(new Intent(this, SignUpPageActivity.class));
+                break;
+        }
     }
 }
