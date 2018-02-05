7464c2b49a55caf3a984cb3c42c601f35b5ffa42
christinatao31 <christinatao31@gmail.com>
2018-02-05 14:41:54 -0800

    added fb signup button

--- app/src/main/java/edu/ucsb/cs48/lookup/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/MainActivity.java
@@ -2,12 +2,16 @@ package edu.ucsb.cs48.lookup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.CallbackManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
     }
 }
