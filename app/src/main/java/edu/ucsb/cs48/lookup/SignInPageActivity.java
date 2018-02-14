f898e55258cd471e8e25b174179c3139be83b1e1
Deni Stoyanova <denitza@umail.ucsb.edu>
2018-02-13 23:32:09 -0800

    facebook button typecasted, import statements added

--- app/src/main/java/edu/ucsb/cs48/lookup/SignInPageActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/SignInPageActivity.java
@@ -26,6 +26,23 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import android.view.View;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.Arrays;
 
 import static android.content.ContentValues.TAG;
@@ -81,7 +98,7 @@ public class SignInPageActivity extends Activity implements View.OnClickListener
 
 
 
-        LoginButton loginButton = findViewById(R.id.fb_login_button);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.fb_login_button); //TODO: why this happen ??!!??!
         info = (TextView)findViewById(R.id.info);
 //        loginButton = (LoginButton) findViewById(R.id.fb_login_button);
         loginButton.setReadPermissions(Arrays.asList(EMAIL));
