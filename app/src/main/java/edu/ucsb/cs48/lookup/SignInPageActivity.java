8a13306957a751d2fef8e9865a226db27740bf88
Erick Suarez <mr.ericksuarez@gmail.com>
2018-02-13 20:25:22 -0800

    merge

--- app/src/main/java/edu/ucsb/cs48/lookup/SignInPageActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/SignInPageActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
