6a5599e7fa1110da02069dd1fbaed7dc9fc9b178
christinatao31 <christinatao31@gmail.com>
2018-02-22 19:18:56 -0800

    complete

--- app/src/main/java/edu/ucsb/cs48/lookup/SignUpPageActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/SignUpPageActivity.java
@@ -13,6 +13,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -23,12 +30,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
+import java.util.Arrays;
 
 import static android.content.ContentValues.TAG;
 
@@ -47,9 +57,12 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
     private FirebaseAuth mAuth;
     private static final int SIGN_IN_REQUEST = 0;
     private Button buttonSignUp;
+    private CallbackManager callbackManager;
 
     private DatabaseReference db;
 
+    private static final String EMAIL = "email";
+    private TextView info;
 
     GoogleSignInClient mGoogleSignInClient;
 
@@ -60,8 +73,11 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        // Check if User is Authenticated
         mAuth = FirebaseAuth.getInstance();
- 
+        FirebaseUser currUser = mAuth.getCurrentUser();
+        updateUI(currUser);
 
         setContentView(R.layout.sign_up_page);
 
@@ -89,6 +105,38 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        // Facebook sign up
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.fb_sign_up_button); //TODO: why this happen ??!!??!
+        info = (TextView)findViewById(R.id.info);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                setResult(RESULT_OK);
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+//                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                setResult(RESULT_CANCELED);
+                Log.d(TAG, "facebook:onCancel");
+                finish();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.d(TAG, "facebook:onError", exception);
+            }
+        });
+
+
     }
 
     //==============================================================================================
@@ -217,6 +265,7 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
@@ -264,6 +313,31 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
                     }
                 });
     }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(SignUpPageActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                    }
+                });
+    }
+
 
 
 }
