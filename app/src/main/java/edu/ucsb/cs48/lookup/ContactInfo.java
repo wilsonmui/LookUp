018cdc5486f86f3b3cb9c57147e6c79877955b25
Erick Suarez <mr.ericksuarez@gmail.com>
2018-02-23 18:45:31 -0800

    Finish writing main class implementation

--- app/src/main/java/edu/ucsb/cs48/lookup/ContactInfo.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/ContactInfo.java
@@ -5,8 +5,44 @@ package edu.ucsb.cs48.lookup;
  */
 
 public abstract class ContactInfo {
-    private String name;
-    public void connect(){
 
+    //==============================================================================================
+    // Fields
+    //==============================================================================================
+
+    private String name;
+
+    //==============================================================================================
+    // Accessor Methods
+    //==============================================================================================
+
+    public String getName() { return this.name; }
+
+    //==============================================================================================
+    // Setter Methods
+    //==============================================================================================
+
+    public void setName(String name) { this.name = name; }
+
+    //==============================================================================================
+    // Methods
+    //==============================================================================================
+    public boolean connect() {
+        // Must be Overriden by children
+        return false;
+    }
+
+    public boolean disconnect() {
+        // Must be Overriden by children
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User) {
+            User o = (User) obj;
+            return this.getName().equals(o.getName());
+        }
+        return false;
     }
 }
