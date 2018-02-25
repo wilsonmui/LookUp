2dc71dd6055507ef7ad692ae5298f0b864997d5e
Deni Stoyanova <deni.stoyanova@gmail.com>
2018-02-24 22:29:05 -0800

    wip face api, app opens successfully

--- app/src/main/java/edu/ucsb/cs48/lookup/CameraActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/CameraActivity.java
@@ -9,10 +9,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+//import com.microsoft.projectoxford.face.*;
+//import com.microsoft.projectoxford.face.contract.*;
 
 public class CameraActivity extends Activity {
     private static final int CAMERA_REQUEST = 1888;
     ImageView imageView;
+//    private FaceServiceClient faceServiceClient =
+//            new FaceServiceRestClient("https://westcentralus.api.cognitive.microsoft.com/face/v1.0", "215542e1a26e4fb499ae0404aa68ed38");
 
     public void onCreate(Bundle savedInstanceState) {
 
