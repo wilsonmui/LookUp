864cea5347361a51db5be9c27e1e9310a0db9dca
Erick Suarez <mr.ericksuarez@gmail.com>
2018-02-26 15:24:28 -0800

    Add Twitter API

--- app/src/main/java/edu/ucsb/cs48/lookup/UserProfileActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/UserProfileActivity.java
@@ -1,13 +1,20 @@
 package edu.ucsb.cs48.lookup;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Created by deni on 2/8/18.
@@ -19,7 +26,10 @@ public class UserProfileActivity  extends AppCompatActivity implements View.OnCl
     // Declare Variables
     //==============================================================================================
     private FirebaseAuth mAuth;
-    private TextView textViewUserEmail, textViewUserName;
+    private TextView textViewUserEmail, textViewUserName, textViewTwitter;
+
+    final String URL_TWITTER_SIGN_IN = "http://androidsmile.com/lab/twitter/sign_in.php";
+    final String URL_TWITTER_GET_USER_TIMELINE = "http://androidsmile.com/lab/twitter/get_user_timeline.php";
 
     //==============================================================================================
     // On Create Setup
@@ -45,6 +55,8 @@ public class UserProfileActivity  extends AppCompatActivity implements View.OnCl
 //        textViewUserEmail.setText(User.getEmail());
 //
 //        loadUserData();
+        textViewTwitter = (TextView) findViewById(R.id.textViewTwitter);
+        signIn();
     }
 
     //==============================================================================================
@@ -72,5 +84,54 @@ public class UserProfileActivity  extends AppCompatActivity implements View.OnCl
 
     }
 
+    /*
+       show dialog with webview to sign in
+    */
+    private void signIn() {
 
+        final Dialog authDialog = new Dialog(this);
+
+        WebView webview = new WebView(this);
+        authDialog.setContentView(webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(URL_TWITTER_SIGN_IN);
+
+        webview.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                if (url.contains("callback.php")) {
+                    view.loadUrl("javascript:JsonViewer.onJsonReceived(document.getElementsByTagName('body')[0].innerHTML);");
+                    authDialog.dismiss();
+                }
+            }
+        });
+        webview.addJavascriptInterface(new MyJavaScriptInterface(getApplicationContext()), "JsonViewer");
+        authDialog.setCancelable(false);
+        authDialog.show();
+
+    }
+
+
+    /*
+        this interface is used to get json from webview
+    */
+    class MyJavaScriptInterface {
+
+        private Context ctx;
+
+        MyJavaScriptInterface(Context ctx) {
+            this.ctx = ctx;
+        }
+
+        @JavascriptInterface
+        public void onJsonReceived(String json) {
+            Gson gson = new GsonBuilder().create();
+            final TwitterOauthResult oauthResult = gson.fromJson(json, TwitterOauthResult.class);
+            if (oauthResult != null && oauthResult.getOauthToken() != null && oauthResult.getOauthTokenSecret() != null) {
+                textViewTwitter.setText(oauthResult.getScreenName());;
+            }
+        }
+    }
 }
