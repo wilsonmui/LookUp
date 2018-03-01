23249cecfa2f1c55a30260a9bc889b70cfab7264
elizamr <eliza_m_ruiz@umail.ucsb.edu>
2018-02-28 21:50:53 -0800

    Added button + page for User Display, Fetching data from Firebase

--- app/src/main/java/edu/ucsb/cs48/lookup/DisplayUsersPageActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/DisplayUsersPageActivity.java
@@ -1,13 +1,32 @@
 package edu.ucsb.cs48.lookup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class DisplayUsersPageActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class DisplayUsersPageActivity extends AppCompatActivity{
+    private FirebaseAuth mAuth;
+    protected TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_users_page);
+        TextView userDisplayName = (TextView)findViewById(R.id.userDisplayName);
+
+        FirebaseUser user = mAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            userDisplayName.setText(user.getDisplayName());
+        }
+        else
+            userDisplayName.setText("User is null!");
     }
+
 }
