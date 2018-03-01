8012e3777d0d89055607d74788bd9fdc8752697e
elizamr <eliza_m_ruiz@umail.ucsb.edu>
2018-03-01 12:10:48 -0800

    Working on diplaying name

--- app/src/main/java/edu/ucsb/cs48/lookup/DisplayUsersPageActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/DisplayUsersPageActivity.java
@@ -8,25 +8,45 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class DisplayUsersPageActivity extends AppCompatActivity{
     private FirebaseAuth mAuth;
-    protected TextView name;
+    private DatabaseReference mDatabase_name, mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_users_page);
-        TextView userDisplayName = (TextView)findViewById(R.id.userDisplayName);
 
-        FirebaseUser user = mAuth.getInstance().getCurrentUser();
+        // Check if User is Authenticated
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
 
-        if (user != null) {
-            userDisplayName.setText(user.getDisplayName());
-        }
-        else
-            userDisplayName.setText("User is null!");
+        // Variable Set up
+        final String user_id = user.getUid();
+        final TextView name;
+        name = findViewById(R.id.userDisplayName);
+        mDatabase_name = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id).child("name");
+
+        // Attempt to display the user's name
+        mDatabase_name.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                name.setText(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 }
