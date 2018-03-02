133d26712a3fd1f7b710db2ab1a42aef9adf55c6
Erick Suarez <mr.ericksuarez@gmail.com>
2018-03-02 11:20:07 -0800

    Add missing semicolon

--- app/src/main/java/edu/ucsb/cs48/lookup/UserProfileActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/UserProfileActivity.java
@@ -2,7 +2,7 @@ package edu.ucsb.cs48.lookup;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
