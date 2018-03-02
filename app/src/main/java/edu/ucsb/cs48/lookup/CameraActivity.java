afdf26869c98fea7d08913b12ba3f7f5ba437cab
Deni Stoyanova <deni.stoyanova@gmail.com>
2018-03-02 13:52:22 -0800

    wip qr scanner

--- app/src/main/java/edu/ucsb/cs48/lookup/CameraActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/CameraActivity.java
@@ -9,16 +9,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.BitmapFactory;
+import android.util.SparseArray;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class CameraActivity extends Activity {
     private static final int CAMERA_REQUEST = 1888;
     ImageView imageView;
+    Bitmap bitmapPhoto;
+    TextView uid;
 
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_activity_page); //TODO
 
+        uid = this.findViewById(R.id.uid_result);
         imageView = (ImageView) this.findViewById(R.id.face_imageview);
         Button photoButton = (Button) this.findViewById(R.id.scan_now);
         Button analyzePhotoButton = (Button) this.findViewById(R.id.analyze_photo);
@@ -35,7 +47,7 @@ public class CameraActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                analyzePhotoWithFaceAPI();
+                analyzeCode();
             }
         });
     }
@@ -43,12 +55,24 @@ public class CameraActivity extends Activity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+            bitmapPhoto = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmapPhoto);
         }
     }
 
-    private void analyzePhotoWithFaceAPI(){
-        //TODO stub
+    private void analyzeCode(){
+        BarcodeDetector detector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+        if(!detector.isOperational()){ //TODO log error!
+            return;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(bitmapPhoto).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        Barcode thisCode = barcodes.valueAt(0);
+        uid.setText(thisCode.rawValue);
     }
 }
