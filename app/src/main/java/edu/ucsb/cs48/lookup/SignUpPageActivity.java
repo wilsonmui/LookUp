a4922ef6434582d0ebcc832afe731cd28e1dafe0
christinatao31 <christinatao31@gmail.com>
2018-03-13 20:24:02 -0700

    working on camera

--- app/src/main/java/edu/ucsb/cs48/lookup/SignUpPageActivity.java
+++ app/src/main/java/edu/ucsb/cs48/lookup/SignUpPageActivity.java
@@ -23,12 +23,14 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Patterns;
@@ -83,10 +85,13 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -116,7 +121,7 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
 
     FirebaseUser user;
 
-    private String fbID;
+    private String fbID, mCurrentPhotoPath;
 
     private static final String NAME = "public_profile", EMAIL = "email";
     private TextView info;
@@ -322,9 +327,30 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
                             requestPermissionCamera();
                         }
                         else {
-                            Log.d(TAG, "wy isn't this working ");
-                            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+                            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            // Ensure that there's a camera activity to handle the intent
+                            if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+                                // Create the File where the photo should go
+                                File photoFile = null;
+                                try {
+                                    photoFile = createImageFile();
+                                } catch (IOException ex) {
+                                    // Error occurred while creating the File
+                                }
+                                // Continue only if the File was successfully created
+                                if (photoFile != null) {
+                                    Uri photoURI = FileProvider.getUriForFile(SignUpPageActivity.this,
+                                            "com.example.android.fileprovider",
+                                            photoFile);
+                                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                                }
+
+                            }
+//                            Log.d(TAG, "wy isn't this working ");
+//                            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//                            startActivityForResult(cameraIntent, CAMERA_REQUEST);
                             setProfilePicPopup.dismiss();
                         }
                     }
@@ -536,20 +562,28 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
         }
 
         // for uploading from camera
-        if (requestCode == CAMERA_REQUEST) {
-                Log.d(TAG, "UHHHHEHHHELELOOOOOO");
-                Uri imageFilePathUri = data.getData();
-                Log.d(TAG, "suck my DICK " + imageFilePathUri);
-                try {
-                    Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageFilePathUri), null, null);
-                    Log.d(TAG, imageFilePathUri.toString());
-                    userProfilePic = rotateBitmap(getApplicationContext(), imageFilePathUri, bitmap, CAMERA);
-                    user_profile_photo.setImageBitmap(userProfilePic);
-                } catch (IOException e) {
-            }
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+//                Log.d(TAG, "UHHHHEHHHELELOOOOOO");
+//                Uri imageFilePathUri = data.getData();
+//                Log.d(TAG, "suck my DICK " + imageFilePathUri);
+//                try {
+//                    Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageFilePathUri), null, null);
+//                    Log.d(TAG, imageFilePathUri.toString());
+//                    userProfilePic = rotateBitmap(getApplicationContext(), imageFilePathUri, bitmap, CAMERA);
+//                    user_profile_photo.setImageBitmap(userProfilePic);
+//                } catch (IOException e) {
+//            }
+            galleryAddPic();
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+//            try {
+//                userProfilePic = rotateBitmap(getApplicationContext(), imageFilePathUri, bitmap, CAMERA);
+//                user_profile_photo.setImageBitmap(userProfilePic);
+//            }
+//            catch (IOException e) {}
+            user_profile_photo.setImageBitmap(bitmap);
         }
     }
-
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
@@ -848,6 +882,30 @@ public class SignUpPageActivity extends AppCompatActivity implements View.OnClic
         return ContextCompat.checkSelfPermission(SignUpPageActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 == PackageManager.PERMISSION_GRANTED;
 
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "LookUp_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 
 }
